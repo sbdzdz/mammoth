Fix masking

models/lwf.py
@@ -8,18 +8,27 @@ from datasets import get_dataset
 from torch.optim import SGD
 
 from models.utils.continual_model import ContinualModel
-from utils.args import add_management_args, add_experiment_args, add_rehearsal_args, ArgumentParser
+from utils.args import (
+    add_management_args,
+    add_experiment_args,
+    add_rehearsal_args,
+    ArgumentParser,
+)
 
 
 def get_parser() -> ArgumentParser:
-    parser = ArgumentParser(description='Continual learning via'
-                                        ' Learning without Forgetting.')
+    parser = ArgumentParser(
+        description="Continual learning via" " Learning without Forgetting."
+    )
     add_management_args(parser)
     add_experiment_args(parser)
-    parser.add_argument('--alpha', type=float, default=0.5,
-                        help='Penalty weight.')
-    parser.add_argument('--softmax_temp', type=float, default=2,
-                        help='Temperature of the softmax function.')
+    parser.add_argument("--alpha", type=float, default=0.5, help="Penalty weight.")
+    parser.add_argument(
+        "--softmax_temp",
+        type=float,
+        default=2,
+        help="Temperature of the softmax function.",
+    )
     return parser
 
 
@@ -33,8 +42,8 @@ def modified_kl_div(old, new):
 
 
 class Lwf(ContinualModel):
-    NAME = 'lwf'
-    COMPATIBILITY = ['class-il', 'task-il']
+    NAME = "lwf"
+    COMPATIBILITY = ["class-il", "task-il"]
 
     def __init__(self, backbone, loss, args, transform):
         super(Lwf, self).__init__(backbone, loss, args, transform)
@@ -44,8 +53,25 @@ class Lwf(ContinualModel):
         self.dataset = get_dataset(args)
         self.current_task = 0
         self.cpt = get_dataset(args).N_CLASSES_PER_TASK
-        nc = get_dataset(args).N_TASKS * self.cpt
-        self.eye = torch.tril(torch.ones((nc, nc))).bool().to(self.device)
+        self.nc = get_dataset(args).N_TASKS * self.cpt
+        self.eye = torch.tril(torch.ones(self.nc, self.nc)).bool()
+
+    def get_lower_triangular_row(self, n):
+        """
+        Generates the n-th row of a lower triangular matrix of ones of size (nc, nc).
+
+        Args:
+        n (int): The row index (0-based).
+        nc (int): The number of columns (and rows) in the matrix.
+
+        Returns:
+        torch.Tensor: The n-th row of the lower triangular matrix.
+        """
+        if n >= self.nc or n < 0:
+            raise ValueError("Row index out of bounds.")
+        row = torch.zeros(self.nc).bool()
+        row[: n + 1] = True
+        return row
 
     def begin_task(self, dataset):
         self.net.eval()
@@ -54,12 +80,20 @@ class Lwf(ContinualModel):
             opt = SGD(self.net.classifier.parameters(), lr=self.args.lr)
             for epoch in range(self.args.n_epochs):
                 for i, data in enumerate(dataset.train_loader):
-                    inputs, labels, not_aug_inputs = data
-                    inputs, labels = inputs.to(self.device), labels.to(self.device)
+                    inputs, labels = data
+                    labels = labels.shape_id
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device, dtype=torch.long)
                     opt.zero_grad()
                     with torch.no_grad():
-                        feats = self.net(inputs, returnt='features')
-                    mask = self.eye[(self.current_task + 1) * self.cpt - 1] ^ self.eye[self.current_task * self.cpt - 1]
+                        feats = self.net(inputs, returnt="features")
+                    mask = (
+                        self.eye[(self.current_task + 1) * self.cpt - 1]
+                        ^ self.eye[self.current_task * self.cpt - 1]
+                    ).to(self.device)
+                    # mask = self.get_lower_triangular_row(
+                    #    (self.current_task + 1) * self.cpt - 1
+                    # ) ^ self.get_lower_triangular_row(self.current_task * self.cpt - 1)
                     outputs = self.net.classifier(feats)[:, mask]
                     loss = self.loss(outputs, labels - self.current_task * self.cpt)
                     loss.backward()
@@ -67,13 +101,24 @@ class Lwf(ContinualModel):
 
             logits = []
             with torch.no_grad():
-                for i in range(0, dataset.train_loader.dataset.data.shape[0], self.args.batch_size):
-                    inputs = torch.stack([dataset.train_loader.dataset.__getitem__(j)[2]
-                                          for j in range(i, min(i + self.args.batch_size,
-                                                         len(dataset.train_loader.dataset)))])
+                for i in range(
+                    0, dataset.train_loader.dataset.data.shape[0], self.args.batch_size
+                ):
+                    inputs = torch.stack(
+                        [
+                            dataset.train_loader.dataset.__getitem__(j)[2]
+                            for j in range(
+                                i,
+                                min(
+                                    i + self.args.batch_size,
+                                    len(dataset.train_loader.dataset),
+                                ),
+                            )
+                        ]
+                    )
                     log = self.net(inputs.to(self.device)).cpu()
                     logits.append(log)
-            setattr(dataset.train_loader.dataset, 'logits', torch.cat(logits))
+            setattr(dataset.train_loader.dataset, "logits", torch.cat(logits))
         self.net.train()
 
         self.current_task += 1
@@ -82,12 +127,16 @@ class Lwf(ContinualModel):
         self.opt.zero_grad()
         outputs = self.net(inputs)
 
-        mask = self.eye[self.current_task * self.cpt - 1]
+        mask = self.eye[self.current_task * self.cpt - 1].to(self.device)
+        # mask = self.get_lower_triangular_row(self.current_task * self.cpt - 1)
         loss = self.loss(outputs[:, mask], labels)
         if logits is not None:
-            mask = self.eye[(self.current_task - 1) * self.cpt - 1]
-            loss += self.args.alpha * modified_kl_div(smooth(self.soft(logits[:, mask]).to(self.device), 2, 1),
-                                                      smooth(self.soft(outputs[:, mask]), 2, 1))
+            mask = self.eye[(self.current_task - 1) * self.cpt - 1].to(self.device)
+            # mask = self.get_lower_triangular_row((self.current_task - 1) * self.cpt - 1)
+            loss += self.args.alpha * modified_kl_div(
+                smooth(self.soft(logits[:, mask]).to(self.device), 2, 1),
+                smooth(self.soft(outputs[:, mask]), 2, 1),
+            )
 
         loss.backward()
         self.opt.step()