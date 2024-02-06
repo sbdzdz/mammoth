40160881a628fcb579439aebb5f50415d12051d5
Sebastian <sebastian.dziadzio@gmail.com>
2024-02-06 23:24:20 +0100

    Rename codis to disco

--- utils/args.py
+++ utils/args.py
@@ -88,7 +88,7 @@ def add_management_args(parser: ArgumentParser) -> None:
     parser.add_argument("--wandb_name", type=str, help="Wandb project name")
     parser.add_argument("--wandb_group", type=str, help="Wandb group")
     parser.add_argument(
-        "--wandb_project", type=str, default="codis", help="Wandb project name"
+        "--wandb_project", type=str, default="disco", help="Wandb project name"
     )
 
 
